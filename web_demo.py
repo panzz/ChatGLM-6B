557443c519851ceb7c5bbc66b113032d2585db0c
Pan LI <lipan@legion.com>
2023-04-11 16:17:12 +0800

    perf: update for dev

--- web_demo.py
+++ web_demo.py
@@ -2,8 +2,9 @@ from transformers import AutoModel, AutoTokenizer
 import gradio as gr
 import mdtex2html
 
-tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True)
-model = AutoModel.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True).half().cuda()
+tokenizer = AutoTokenizer.from_pretrained(".\THUDM\chatglm-6b", trust_remote_code=True)
+# model = AutoModel.from_pretrained(".\THUDM\chatglm-6b", trust_remote_code=True).half().cuda()
+model = AutoModel.from_pretrained(".\THUDM\chatglm-6b", trust_remote_code=True).half().quantize(4).cuda()
 model = model.eval()
 
 """Override Chatbot.postprocess"""
